a5562c74ef201a4493ad84d1df8229af8f4ef4d3
mvonpohle <mvonpohle@gmail.com>
2018-11-17 06:30:57 -0800

    modify_ntp and support functions complete

--- ntp_spoof.py
+++ ntp_spoof.py
@@ -7,12 +7,12 @@ Task Division:
 #3 & #4 - Check_ntp(/*packet from netfilter queue) - Karthik
 	-Checks that ntp packet & that it's an ntp server response
 	-Passes ntp packet to modify_ntp()
-	-Checks return if 0 or packet
+	-Receives modified packet from modify_ntp()
 	-Handles udp manipulation - updating checksum
 	-Returns udp packet to netfilterqueue
 #5 - Modfy_ntp(/*ntp packet */) - Michael
 	-Modifies the appropriate ntp packet fields
-	-Returns ntp packet or 0 if something went wrong
+	-Returns ntp packet
 #1 & #2 - main() - siddharth
 	-Setup arp spoofing, ip forwarding, kamene(scapy), netfilterqueue
 	-Print status messages handle clean up
@@ -26,14 +26,13 @@ import sys
 from subprocess import Popen, DEVNULL
 # from netfilterqueue import NetfilterQueue
 from kamene.all import *
-from datetime import datetime
+import datetime
 from math import modf
 
-unit_conversion_to_seconds = {'microsecond': .000001, 'minutes': 60, 'hours': 3600,
-                              'days': 3600 * 24, 'years': 3600 * 24 * 365}
-
-adjustment = 2
-adjustment_unit = 'years'
+NTP_OFFSET = (70 * 365 + 17) * 86400
+TIME_ADJUST_BY = {'days': 5, 'hours': -3}
+TIME_ADJUST_FIELDS = {'year': 2017, 'month': 10}
+TIME_ASSIGN = {'year': 2018, 'month': 11, 'day': 10, 'hour': 11, 'minute': 11, 'second': 11}
 
 
 def check_ntp():  # argument is packet from netfilter queue
@@ -42,32 +41,70 @@ def check_ntp():  # argument is packet from netfilter queue
 
 
 def modify_ntp(ntp_packet):  # argument is ntp payload
-    # fields to change:
-    # -reference, originate, receive by the same offset
-    # for testing
-    # ntp_packet.ref = 3750257481.2553115  # tis a float
-    # for testing
-    ntp_packet.ref = adjust_ntp_time_by(ntp_packet.ref, adjustment, adjustment_unit)
-    ntp_packet.orig = adjust_ntp_time_by(ntp_packet.orig, adjustment, adjustment_unit)
-    ntp_packet.recv = adjust_ntp_time_by(ntp_packet.recv, adjustment, adjustment_unit)
+    """
+    Adjusts the ntp_packet time fields using three different methods
+    :param ntp_packet: kamene ntp packet
+    :return:
+    """
+    ntp_packet.ref = adjust_ntp_time_by(ntp_packet.ref, datetime.timedelta(**TIME_ADJUST_BY))
+    #ntp_packet.ref = adjust_ntp_time_fields(ntp_packet.ref, TIME_ADJUST_FIELDS)
+    #ntp_packet.ref = posix_datetime_to_ntp_timestamp(datetime.datetime(**TIME_ASSIGN))
+    ntp_packet.orig = adjust_ntp_time_by(ntp_packet.orig, datetime.timedelta(**TIME_ADJUST_BY))
+    ntp_packet.recv = adjust_ntp_time_by(ntp_packet.recv, datetime.timedelta(**TIME_ADJUST_BY))
     return ntp_packet
 
-def adjust_ntp_time_by(ntp_timestamp, adjustment, adjustment_unit):
-    '''
-    Adjust ntp_timestamp by the adjustment value for the given adjustment unit
+
+def adjust_ntp_time_by(ntp_timestamp, timedelta):
+    """
+    Adjusts the ntp timestamp by the supplied delta amount
     :param ntp_timestamp: float
-    :param adjustment: int
-    :param adjustment_unit: one of the arguments of datetime.datetime, except months
-    :return: modified float datetime
-    '''
-    if adjustment_unit in unit_conversion_to_seconds:
-        return ntp_timestamp + adjustment * unit_conversion_to_seconds[adjustment_unit]
-    else:
-        print("ERROR: adjustment_unit not supported. timestamp not converted")
-        return ntp_timestamp
-    # timestamp = modf(ntp_timestamp)
-    # print(timestamp)
-    # datetime()
+    :param timedelta: datetime.timedelta object
+    :return:
+    """
+    posix_datetime = ntp_timestamp_to_posix_datetime(ntp_timestamp)
+    print(posix_datetime)
+    adjusted_posix_datetime = posix_datetime + timedelta
+    print(adjusted_posix_datetime)
+    return posix_datetime_to_ntp_timestamp(adjusted_posix_datetime)
+
+
+def adjust_ntp_time_fields(ntp_timestamp, adjust_dict):
+    """
+    Adjusts the ntp time values only for the fields specified in adjust_dict
+    :param ntp_timestamp: float
+    :param adjust_dict: dictionary of datetime fields to adjust
+    :return:
+    """
+    posix_datetime = ntp_timestamp_to_posix_datetime(ntp_timestamp)
+    print(posix_datetime)
+    adjusted_posix_datetime = datetime.datetime(
+        adjust_dict['year'] if 'year' in adjust_dict else posix_datetime.year,
+        adjust_dict['month'] if 'month' in adjust_dict else posix_datetime.month,
+        adjust_dict['day'] if 'day' in adjust_dict else posix_datetime.day,
+        adjust_dict['hour'] if 'hour' in adjust_dict else posix_datetime.hour,
+        adjust_dict['minute'] if 'minute' in adjust_dict else posix_datetime.minute,
+        adjust_dict['second'] if 'second' in adjust_dict else posix_datetime.second,
+        adjust_dict['microsecond'] if 'microsecond' in adjust_dict else posix_datetime.microsecond)
+    print(adjusted_posix_datetime)
+    return posix_datetime_to_ntp_timestamp(adjusted_posix_datetime)
+
+
+def posix_datetime_to_ntp_timestamp(input_datetime):
+    """
+    Converts the posix datetime object to an ntp_timestamp
+    :param input_datetime: datetime.datetime object
+    :return:
+    """
+    return input_datetime.timestamp() + NTP_OFFSET
+
+
+def ntp_timestamp_to_posix_datetime(ntp_timestamp):
+    """
+    Converts ntp_timestamp to posix datetime object
+    :param ntp_timestamp:
+    :return:
+    """
+    return datetime.datetime(1, 1, 1).fromtimestamp(ntp_timestamp - NTP_OFFSET)
 
 
 def main():  # no arguments
